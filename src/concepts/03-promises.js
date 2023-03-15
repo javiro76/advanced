fac91cfb3936c981cd78394e7fe7824fda3485ba
javiro76 <javiro76@gmail.com>
2023-03-14 21:35:08 -0500

    se realiza .then y .catch

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -9,8 +9,28 @@ export const promisesComponent = ( element ) => {
 
   console.log('promisesComponent');
 
+  const renderHero = ( hero ) => {
+
+    element.innerHTML = hero.name;    
+  }
+
+  const renderError = ( error ) => {
+    element.innerHTML = `
+        <h1>Error:</h1>
+        <h3>${ error }</h3>
+    `
+  }
+
+  const id1 = "5d86371f9f80b591f499df32";
+
+  findHero( id1 )
+    .then( renderHero )
+    .catch( renderError );
 
 }
+
+
+
 
 
 /**
