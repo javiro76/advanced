bf85627098e9bbb6d85d9ca489ba5f5986f44084
javiro76 <javiro76@gmail.com>
2023-03-15 19:13:44 -0500

    tres formas de realizar las promesas

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -14,6 +14,14 @@ export const promisesComponent = ( element ) => {
     element.innerHTML = hero.name;    
   }
 
+
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+        <h3>${hero1.name}</h3>
+        <h3>${hero2.name}</h3>
+    `;    
+  }
+
   const renderError = ( error ) => {
     element.innerHTML = `
         <h1>Error:</h1>
@@ -21,11 +29,54 @@ export const promisesComponent = ( element ) => {
     `
   }
 
-  const id1 = "5d86371f9f80b591f499df32";
 
-  findHero( id1 )
-    .then( renderHero )
-    .catch( renderError );
+
+  const id1 = "5d86371f9f80b591f499df32";
+  const id2 = "5d86371f233c9f2425f16916";
+
+  
+  //----FORMA 3 ------
+  // Esta forma se utiliza cuando las promesas no dependen una de otra
+  Promise.all([
+      findHero(id1),
+      findHero(id2)
+  ])
+  .then(([hero1, hero2]) => renderTwoHeroes( hero1, hero2 ))
+  .catch( renderError );
+
+
+
+
+
+
+
+  // -----FORMA 2---------
+  // let hero1;
+
+  // findHero( id1 )
+  //   .then( hero => {
+  //     hero1 = hero; 
+  //     return findHero(id2);
+  //   }).then( hero2 => {
+  //       renderTwoHeroes( hero1, hero2 );
+  //   })
+  //   .catch( renderError );
+
+
+  // -----FORMA 1---------
+  // let hero1;
+  // findHero( id1 )
+  //   .then( (hero1) =>{
+      
+  //     findHero( id2 )
+  //       .then( hero2 =>{
+  //             renderTwoHeroes( hero1, hero2 )
+              
+  //           })
+  //       .catch( renderError );
+
+  //   })
+  //   .catch( renderError );
 
 }
 
