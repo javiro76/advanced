bfeb4f2d206f67ee2ed3c3a3c9b4a06305bf5280
javiro76 <javiro76@gmail.com>
2023-03-27 18:24:21 -0500

    ejemplo de async

---
+++ src/concepts/05-async.js
@@ -0,0 +1,36 @@
+import { heroes } from "../data/heroes"
+
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncComponent = ( element ) => {
+
+  const id1 = '5d86371f97c29d020f1e1f6d';
+
+  findHero( id1 )
+      .then( name  => element.innerHTML = name )
+      .catch( error => element.innerHTML = error);
+
+
+};
+
+
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<String>}
+ */
+
+const findHero = async(id) => {
+
+  const hero = heroes.find( (hero) => hero.id === id );
+
+  return hero.name;
+
+}
+
+
+
